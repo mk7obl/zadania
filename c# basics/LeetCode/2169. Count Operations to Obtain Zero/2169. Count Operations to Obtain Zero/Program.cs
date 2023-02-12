7c072c76ab237362931b031280f549e6c7302e27
mk7obl <mkomsa7@gmail.com>
2023-02-12 15:56:16 +0100

    tasks added

---
+++ c# basics/LeetCode/2169. Count Operations to Obtain Zero/2169. Count Operations to Obtain Zero/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int count = 0;
+
+int CountOperations(int num1, int num2)
+{
+    if ((num1 >= 0) && (num2 <= Math.Pow(10, 5)))    //not necessary
+    {
+        while (num1 > 0 && num2 > 0)
+        {
+            int result = 0;                  //-=, int result not necessary (memory)
+
+            if (num1 >= num2)
+            {
+                result = num1 - num2;        //-= ^
+                num1 = result;
+                count++;
+            }
+
+            else
+            {
+                result = num2 - num1;        //-= ^^
+                num2 = result;
+                count++;
+            }
+
+        };
+    }
+    return count;
+}
+
+CountOperations(0, 3);
+Console.WriteLine(count);
+
+
---
+++ c# basics/LeetCode/2169. Count Operations to Obtain Zero/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Hello, World!");
+
+int num1 = 2; //Convert.ToInt32(Console.ReadLine());
+int num2 = 3; //Convert.ToInt32(Console.ReadLine());
+
+int result = 0;
+int count = 0;
+
+do
+{
+    if (num1 >= num2)
+    {
+        result = num1 - num2;
+        num1 = result;
+        count++;
+    }
+
+    else
+    {
+        result = num2 - num1;
+        num2 = result;
+        count++;
+    }
+} while (num1 > 0 || num2 > 0);
+
+Console.WriteLine(count);
