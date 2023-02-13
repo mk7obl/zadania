e101accf5a27fd1e8f3919802ea0c59e5deb0bc5
mk7obl <mkomsa7@gmail.com>
2023-02-13 15:11:39 +0100

    algorithm study plan

---
+++ c# basics/LeetCode/Algorithms I/278. First Bad Version/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution : VersionControl
+{
+    int index;
+    int low = 1;
+    public int FirstBadVersion(int n)
+    {
+
+        if (IsBadVersion(low) == true)
+        {
+            index = low;
+        }
+
+        if (index != low)
+        {
+            for (int i = n; i >= 0; i--)
+            {
+
+                bool ans = IsBadVersion(i);
+
+                if (ans == false)
+                {
+                    index = i + 1;
+                    break;
+                }
+
+            }
+        }
+        return index;
+    }
+}
---
+++ c# basics/LeetCode/Algorithms I/704. Binary search/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int index = 0; ;
+int Search(int[] nums, int target)
+{
+    foreach (int x in nums)
+    {
+
+        if (x == target)
+        {
+            index = Array.IndexOf(nums, x);
+            break;
+        }
+
+    }
+
+    if (index != Array.IndexOf(nums, target))
+    {
+        index = -1;
+    }
+    return index;
+}
+
+int[] numery = { -1, 0, 3, 5, 9, 12 };
+
+Search(numery, 2);
