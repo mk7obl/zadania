b945d4b73b014250b344fb6855e2506433bd6414
mk7obl <mkomsa7@gmail.com>
2023-02-13 01:55:16 +0100

    tasks added

---
+++ c# basics/LeetCode/2243. Calculate Digit Sum of a String/Program.cs
@@ -0,0 +1,35 @@
+﻿
+//try it with StringBuilder to check runtime & memory
+string result = "";
+int sum = 0;
+string DigitSum(string s, int k)
+{
+    while (s.Length > k)
+    {
+        result = "";
+
+        for (int j = 0; j < s.Length; j += k)
+        {
+            int i;
+            for (i = j; (i < j + k) && (i < s.Length); i++)
+            {
+                sum += Int32.Parse(s[i].ToString());
+            }
+
+            result += sum;
+            sum = 0;
+        }
+
+        s = result;
+
+    }
+
+    if (s.Length <= k)
+        result = s;
+
+    return result;
+}
+
+
+DigitSum("1", 2);
+Console.WriteLine(result);
