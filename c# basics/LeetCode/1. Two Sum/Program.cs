29c32f8a721dd04a36004b2a9e18631b1945bfcd
mk7obl <mkomsa7@gmail.com>
2023-02-14 23:22:17 +0100

    update

---
+++ c# basics/LeetCode/1. Two Sum/Program.cs
@@ -0,0 +1,32 @@
+﻿
+int sum = 0;
+int[] results = new int[2];
+
+int[] TwoSum(int[] nums, int target)
+{
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (nums[i] <= target)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+
+                sum = nums[i] + nums[j];
+
+                if (sum == target && i != j)
+                {
+                    results[0] = i;
+                    results[1] = j;
+                    return results;
+                }
+
+            }
+        }
+    }
+
+    return results;
+}
+int[] cyferki = { 0, 4, 3, 0 };
+TwoSum(cyferki, 0);
+Console.WriteLine(results);
