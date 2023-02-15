62394ef845efe6a012fee88ad675bcd5292a8416
mk7obl <mkomsa7@gmail.com>
2023-02-15 23:16:19 +0100

    update

---
+++ c# basics/LeetCode/203. Remove Linked List Elements/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+/// <summary>
+/// need to come up with own solution
+/// </summary>
+/// 
+//Definition for singly-linked list.
+//public class ListNode
+//{
+//    public int val;
+//    public ListNode next;
+//    public ListNode(int val = 0, ListNode next = null)
+//    {
+//        this.val = val;
+//        this.next = next;
+//    }
+//}
+
+
+public class Solution
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+
+        ListNode tmp = new ListNode(0);
+        tmp.next = head;
+
+        ListNode previous = tmp;
+        ListNode current = head;
+
+        while (current != null)
+        {
+            if (current.val != null)
+            {
+                previous.next = current.next;
+            }
+            else
+                previous = current;
+            current = current.next;
+        }
+
+        return tmp.next;
+    }
+}
---
+++ c# basics/LeetCode/868. Binary Gap/Program.cs
@@ -0,0 +1,70 @@
+﻿
+
+//int BinaryGap(int n)
+//{
+//    string bin = Convert.ToString(n, 2);
+//    int distance = 0;
+
+//    ///                                      value  index  
+//    Dictionary<int, int> dict = new Dictionary<int, int>();
+
+//    for (int i = 0; i < bin.Length; i++)
+//    {
+
+//        if (bin[i] == 1)
+//        {
+//            dict.Add(bin[i], bin.IndexOf(bin[i]));
+//        }
+
+//    }
+
+//    for (int i = 0; i < dict.Count; i++)
+//    {
+
+//        if (distance[i] > distance[i - 1])
+//        {
+//            distance = dict[i + 1] - dict[i];
+//        }
+
+//        Console.WriteLine(distance);
+//    }
+//}
+
+//BinaryGap(22);
+
+/////liczenie do i
+/////liczenie i[j]
+///// i=j
+///// i od nowa
+///// 
+
+using System.Globalization;
+///
+
+int BinaryGap(int n)
+{
+    string dec = Convert.ToString(n, 2);
+    int distance = 0;
+
+    for (int i = 0; i < dec.Length; i++)
+    {
+        if (dec[i] == '1')
+        {
+            for (int j = i+1; j < dec.Length; j++)
+            {
+                if (dec[j+1] == '0')
+                {
+                    distance++;
+                }
+
+                else if (dec[j] == '1')
+                    break;
+                i = j;
+            }
+        }
+    }
+    return distance;
+}
+
+BinaryGap(22);
+
