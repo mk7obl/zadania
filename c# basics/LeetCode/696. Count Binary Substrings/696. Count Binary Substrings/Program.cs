aad7fbd7c920eae0786b83d95f573543fe5c3b07
mk7obl <mkomsa7@gmail.com>
2023-02-19 23:59:52 +0100

    initial commit

---
+++ c# basics/LeetCode/696. Count Binary Substrings/696. Count Binary Substrings/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _696._Count_Binary_Substrings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
