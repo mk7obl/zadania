59c4b68192ca7fe2c91be15e04ed69bc0e436411
mk7obl <mkomsa7@gmail.com>
2023-02-23 17:55:59 +0100

    update

---
+++ c# basics/LeetCode/1641. Count Sorted Vowel Strings/Program.cs
@@ -0,0 +1,59 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+//Given an integer n, return the number of strings of length n that consist only of vowels (a, e, i, o, u) and are lexicographically sorted.
+
+//A string s is lexicographically sorted if for all valid i, s[i] is the same as or comes before s[i+1] in the alphabet.
+
+
+int CountVowelStrings(int n)
+{
+
+    Dictionary<int, string> dict = new Dictionary<int, string>()
+    {
+        {0, "a"},
+        {1, "e"},
+        {2, "i"},
+        {3, "o"},
+        {4, "u"}
+    };
+
+    string result = "";
+    int count = 0;
+
+    if (n == 1)
+        return dict.Count;
+
+    for (int i = 0; i < dict.Count; i++)
+    {
+        for (int j = i; j < dict.Count; j++)
+        {
+            if (n==2)
+                result = dict[i] + dict[j];
+
+            for(int k= j; k < dict.Count;k++)
+            {
+                result += dict[k];
+
+                
+                if (result.Length==n)
+                {
+                    Console.WriteLine(result);
+                    count++;
+                    break; 
+                }
+
+                if (k == dict.Count)
+                    k = 0;
+            }
+
+        }
+    }
+
+    Console.WriteLine(count);
+    return count;
+}
+
+CountVowelStrings(3);
+
+
---
+++ c# basics/LeetCode/2124. Check if All A's Appears Before All B's/Program.cs
@@ -0,0 +1,29 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+
+//Given a string s consisting of only the characters 'a' and 'b', return true if every 'a' appears before every 'b' in the string. Otherwise, return false.
+
+//runtime 91%, memory 20%
+
+bool CheckString(string s)
+{
+    int size = s.Length;
+    int aIndex;
+    int bIndex;
+
+
+    aIndex = s.LastIndexOf('a');
+    bIndex = s.IndexOf('b');
+
+    Console.WriteLine(aIndex);
+    Console.WriteLine(bIndex);
+
+    if (bIndex < aIndex && bIndex != -1)
+        return false;
+
+    return true;
+}
+
+string s = "a";
+CheckString(s);
