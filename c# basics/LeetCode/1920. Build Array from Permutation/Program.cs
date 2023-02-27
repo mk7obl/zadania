f4cd06e4701e981cafde82618e4f537c4ac7b503
mk7obl <mkomsa7@gmail.com>
2023-02-27 22:40:41 +0100

    task update

---
+++ c# basics/LeetCode/1920. Build Array from Permutation/Program.cs
@@ -0,0 +1,20 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+using System.Reflection.Metadata.Ecma335;
+
+int[] BuildArray(int[] nums)
+{
+
+    int[] result = new int[nums.Length];
+    int size = result.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        result[i] = nums[nums[i]];
+    }
+
+    return result;
+}
+
+
